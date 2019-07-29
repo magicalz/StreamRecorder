3ad59b621f0be4be2ad1dd04d184523ee2638aa3
magicalz <magicalz.jerry@gmail.com>
2019-07-30 05:34:37 +0800

    add logfolder, rcloneremotename in global config, bug fix

--- rclonebackup.sh
+++ rclonebackup.sh
@@ -4,18 +4,18 @@ NAME="${1:-all}"
 SITE="$2"
 SERVERNAME=$(grep "Servername" ./config/config.global|awk -F = '{print $2}')
 SAVEFOLDER=$(grep "Savefolder" ./config/config.global|awk -F = '{print $2}')
-[[ ! -d "log" ]]&&mkdir log
+REMOTENAME=$(grep "Rcloneremotename" ./config/config.global|awk -F = '{print $2}')
 LOG_PREFIX=$(date +"[%Y-%m-%d-%H]")
 echo "$LOG_PREFIX rclone begin to backup files"
 echo "$LOG_PREFIX check ./log/rclone_${LOG_PREFIX}.log for detail"
 if [ "$NAME" == "all" ]
 then
-  rclone copy --no-traverse "$SAVEFOLDER" magicalz:${SERVERNAME}/stream-recorder/${SAVEFOLDER} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1
+  rclone copy --no-traverse "$SAVEFOLDER" ${REMOTENAME}:${SERVERNAME}/stream-recorder/${SAVEFOLDER} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1
 elif [ -z "$SITE" ]
 then
-  rclone copy --no-traverse "${SAVEFOLDER}/${NAME}" magicalz:${SERVERNAME}/stream-recorder/${SAVEFOLDER}/${NAME} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1
+  rclone copy --no-traverse "${SAVEFOLDER}/${NAME}" ${REMOTENAME}:${SERVERNAME}/stream-recorder/${SAVEFOLDER}/${NAME} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1
 else
-  rclone copy --no-traverse "${SAVEFOLDER}/${NAME}/${SITE}" magicalz:${SERVERNAME}/stream-recorder/${SAVEFOLDER}/${NAME}/${SITE} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1  
+  rclone copy --no-traverse "${SAVEFOLDER}/${NAME}/${SITE}" ${REMOTENAME}:${SERVERNAME}/stream-recorder/${SAVEFOLDER}/${NAME}/${SITE} --include-from rcloneinclude.txt --buffer-size 32M --transfers 6 --low-level-retries 200 -v > "./log/rclone_${LOG_PREFIX}.log" 2>&1  
 fi
 if grep -q "Copied (new)" ./log/rclone_${LOG_PREFIX}.log
 then
